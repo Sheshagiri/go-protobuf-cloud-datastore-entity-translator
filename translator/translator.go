b47610e65d5cdc3a0d029ce3f8c5082513df5c68
Sheshagiri Rao Mallipedhi <shmallipedhi@extremenetworks.com>
2019-06-30 23:46:31 +0530

    clean up and added DatastoreEntityToProtoMessage

--- translator/translator.go
+++ translator/translator.go
@@ -1,94 +1,16 @@
 package translator
 
 import (
-	"fmt"
-	"github.com/golang/protobuf/ptypes/struct"
-	"reflect"
-	//"github.com/golang/protobuf/ptypes/timestamp"
-	dbv2 "google.golang.org/api/datastore/v1"
-	//dbv2 "google.golang.org/appengine/datastore"
-	//"github.com/Sheshagiri/protobuf-struct/models"
 	"errors"
+	"fmt"
 	"github.com/golang/protobuf/proto"
+	dbv2 "google.golang.org/api/datastore/v1"
+	"reflect"
 	"strings"
 )
 
-// TranslateToDatastore will transform an interface to Entity Protobuf that Datastore understands
-func TranslateToDatastore(src interface{}) {
-
-	t := reflect.TypeOf(src)
-	//v := reflect.ValueOf(src)
-	for i := 0; i < t.NumField(); i++ {
-		fmt.Printf("%+v\n", t.Field(i))
-		if t.Field(i).Type.Kind() == reflect.TypeOf(structpb.Struct{}).Kind() {
-			fmt.Println("yeyy")
-		}
-	}
-}
-
-func ProtoToEntity(src proto.Message) dbv2.Entity {
-	srcValues := reflect.ValueOf(src).Elem()
-	dst := dbv2.Entity{}
-	properties := make(map[string]dbv2.Value)
-	for i := 0; i < srcValues.NumField(); i++ {
-		name := srcValues.Type().Field(i).Name
-		if !strings.Contains(name, "XXX_") {
-			value := srcValues.Field(i).Interface()
-			//fmt.Printf("Type: %v, Name: %v, Value: %v\n", l.Field(i).Type(), name, value)
-			fmt.Println(srcValues.Type().Field(i).Type.String())
-			switch srcValues.Type().Field(i).Type.String() {
-			case "bool":
-				properties[name] = dbv2.Value{
-					BooleanValue: value.(bool),
-				}
-			case "string":
-				fmt.Println("string value")
-				properties[name] = dbv2.Value{
-					StringValue: fmt.Sprint(value),
-				}
-			case "*timestamp.Timestamp":
-				fmt.Println("*timestamp.Timestamp")
-				properties[name] = dbv2.Value{
-					TimestampValue: fmt.Sprint(value),
-				}
-			case "*structpb.Struct":
-				fmt.Println("convert to *structpb.Struct")
-			}
-		}
-	}
-	dst.Properties = properties
-	return dst
-}
-
-func EntityToProto(src dbv2.Entity, dst proto.Message) {
-	dstValues := reflect.ValueOf(dst).Elem()
-	for i := 0; i < dstValues.NumField(); i++ {
-		fieldName := dstValues.Type().Field(i).Name
-		if !strings.Contains(fieldName, "XXX_") {
-			fieldValue := getProperty(src.Properties, fieldName)
-			fmt.Println(dstValues.Type().Field(i).Type.String())
-			switch dstValues.Type().Field(i).Type.String() {
-			case "string":
-				fmt.Println("type: string")
-				fmt.Println(fieldValue)
-				dstValues.Field(i).SetString(fmt.Sprint(fieldValue))
-			case "*timestamp.Timestamp":
-				fmt.Println(fieldValue)
-			case "*structpb.Struct":
-				fmt.Println("convert to *structpb.Struct")
-			}
-		}
-	}
-}
-
 func getProperty(properties map[string]dbv2.Value, name string) dbv2.Value {
 	return properties[name]
-}
-
-func getValue(src interface{}, field string) {
-	r := reflect.ValueOf(src)
-	f := reflect.Indirect(r).FieldByName(field)
-	fmt.Println(f.Interface())
 }
 
 // ProtoMessageToDatastoreEntity will generate an Entity Protobuf that datastore understands
@@ -112,9 +34,31 @@ func ProtoMessageToDatastoreEntity(src proto.Message) dbv2.Entity {
 	}
 	entity.Properties = properties
 	fmt.Println("@@@@@@@")
-	fmt.Printf("Entity: %v\n",entity)
+	fmt.Printf("Entity: %v\n", entity)
 	fmt.Println("@@@@@@@")
 	return entity
+}
+
+func DatastoreEntityToProtoMessage(src dbv2.Entity, dst proto.Message) {
+	dstValues := reflect.ValueOf(dst).Elem()
+	for i := 0; i < dstValues.NumField(); i++ {
+		fName := dstValues.Type().Field(i).Name
+		if !strings.Contains(fName, "XXX_") {
+			fValue := getProperty(src.Properties, fName)
+			fType := dstValues.Type().Field(i).Type.String()
+			fmt.Printf("name: %s, type: %s\n", fName, fType)
+			switch fType {
+			case "string":
+				dstValues.Field(i).SetString(fValue.StringValue)
+			case "bool":
+				dstValues.Field(i).SetBool(fValue.BooleanValue)
+			case "int32", "int64":
+				dstValues.Field(i).SetInt(fValue.IntegerValue)
+			case "float32", "float64":
+				dstValues.Field(i).SetFloat(fValue.DoubleValue)
+			}
+		}
+	}
 }
 
 func toValue(fType string, fValue reflect.Value) (value dbv2.Value, err error) {
@@ -128,19 +72,19 @@ func toValue(fType string, fValue reflect.Value) (value dbv2.Value, err error) {
 	case "float32", "float64":
 		value.DoubleValue = fValue.Float()
 	case "slice":
-		//TODO add complex type to the slice
+		//TODO add complex type to the slicell
 		if fValue.Type().Elem().Kind() == reflect.Uint8 {
 			//BlobValue is a string in the datastore entity proto
 			value.BlobValue = string(fValue.Bytes())
 		} else {
 			size := fValue.Len()
-			values := make([]*dbv2.Value,size)
-			for i := 0; i < size ; i++ {
-				val, _ := toValue(fValue.Type().Elem().Kind().String(),fValue.Index(i))
+			values := make([]*dbv2.Value, size)
+			for i := 0; i < size; i++ {
+				val, _ := toValue(fValue.Type().Elem().Kind().String(), fValue.Index(i))
 				values[i] = &val
 			}
 			value.ArrayValue = &dbv2.ArrayValue{
-				Values:values,
+				Values: values,
 			}
 		}
 	case "map":
--- translator/translator_test.go
+++ translator/translator_test.go
@@ -1,49 +1,90 @@
 package translator
 
 import (
-	"fmt"
-	"github.com/Sheshagiri/protobuf-struct/models/execution"
-	"github.com/golang/protobuf/ptypes"
+	"github.com/Sheshagiri/protobuf-struct/models/example"
 	"github.com/stretchr/testify/assert"
-	dbv2 "google.golang.org/api/datastore/v1"
+	"google.golang.org/api/datastore/v1"
 	"testing"
 )
 
-func TestTranslateToDatastore(t *testing.T) {
-	p := execution.ExecutionRequest{
-		Action: "a1",
-		Uuid:   "some-uuid",
+func TestProtoMessageToDatastoreEntitySimple(t *testing.T) {
+	example := &example.ExampleNestedModel{
+		StringKey: "some random string",
+		Int32Key:  22,
 	}
-	TranslateToDatastore(p)
+	entity := ProtoMessageToDatastoreEntity(example)
+
+	assert.Equal(t, "some random string", entity.Properties["StringKey"].StringValue)
+	assert.Equal(t, 22, int(entity.Properties["Int32Key"].IntegerValue))
 }
 
-func TestProtoToEntity(t *testing.T) {
-	p := &execution.ExecutionRequest{
-		Action:    "validate",
-		Uuid:      "some-random-uuid",
-		StartedOn: ptypes.TimestampNow(),
+func TestProtoMessageToDatastoreEntityComplex(t *testing.T) {
+	float32 := float32(10.1)
+	e1 := &example.ExampleDBModel{
+		StringKey: "some random string key for testing",
+		BoolKey:   true,
+		Int32Key:  32,
+		Int64Key:  64,
+		FloatKey:  float32,
+		DoubleKey: float64(10.2121),
+		BytesKey:  []byte("this is a byte array"),
+		StringArrayKey: []string{
+			"element-1",
+			"element-2",
+		},
+		Int32ArrayKey: []int32{
+			1, 2, 3, 4, 5, 6,
+		},
 	}
-	entity := ProtoToEntity(p)
-	assert.NotNil(t, entity.Properties)
-	fmt.Println(entity)
+	/*MapStringInt32:map[string]int32{
+		"int-key-1":1,
+		"int-key-2":2,
+	},
+	MapStringString:map[string]string{
+		"string-key-1":"k1",
+		"string-key-2":"k2",
+	},*/
+	entity := ProtoMessageToDatastoreEntity(e1)
+
+	assert.Equal(t, "some random string key for testing", entity.Properties["StringKey"].StringValue)
+	assert.Equal(t, true, entity.Properties["BoolKey"].BooleanValue)
+	assert.Equal(t, int64(32), entity.Properties["Int32Key"].IntegerValue)
+	assert.Equal(t, int64(64), entity.Properties["Int64Key"].IntegerValue)
+	assert.Equal(t, float64(float32), entity.Properties["FloatKey"].DoubleValue)
+	assert.Equal(t, float64(10.2121), entity.Properties["DoubleKey"].DoubleValue)
+	//TODO BlobValue returns a string
+	assert.Equal(t, string([]byte("this is a byte array")), entity.Properties["BytesKey"].BlobValue)
+	//assert string array
+	assert.Equal(t, "element-1", entity.Properties["StringArrayKey"].ArrayValue.Values[0].StringValue)
+	assert.Equal(t, "element-2", entity.Properties["StringArrayKey"].ArrayValue.Values[1].StringValue)
+	//assert int32 array
+	assert.Equal(t, int64(1), entity.Properties["Int32ArrayKey"].ArrayValue.Values[0].IntegerValue)
+	assert.Equal(t, int64(3), entity.Properties["Int32ArrayKey"].ArrayValue.Values[2].IntegerValue)
+	assert.Equal(t, int64(5), entity.Properties["Int32ArrayKey"].ArrayValue.Values[4].IntegerValue)
+	assert.Equal(t, int64(6), entity.Properties["Int32ArrayKey"].ArrayValue.Values[5].IntegerValue)
 }
 
-func TestEntityToProto(t *testing.T) {
-	entity := dbv2.Entity{}
-	var properties map[string]dbv2.Value
-
-	properties["Action"] = dbv2.Value{
-		StringValue: "action-1",
+func TestDatastoreEntityToProtoMessage(t *testing.T) {
+	properties := make(map[string]datastore.Value)
+	properties["StringKey"] = datastore.Value{
+		StringValue: "some random string key",
 	}
-	properties["Uuid"] = dbv2.Value{
-		StringValue: "some-random-uuid",
+	properties["Int64Key"] = datastore.Value{
+		IntegerValue: 64,
 	}
-	properties["StartedOn"] = dbv2.Value{
-		TimestampValue: ptypes.TimestampString(ptypes.TimestampNow()),
+	properties["DoubleKey"] = datastore.Value{
+		DoubleValue: float64(64),
 	}
-	entity.Properties = properties
-	execRequest := &execution.ExecutionRequest{}
-	EntityToProto(entity, execRequest)
-	assert.Equal(t, "action-1", execRequest.GetAction())
-	assert.Equal(t, "some-random-uuid", execRequest.GetUuid())
+	properties["BoolKey"] = datastore.Value{
+		BooleanValue: false,
+	}
+	entity := datastore.Entity{
+		Properties: properties,
+	}
+	example := &example.ExampleDBModel{}
+	DatastoreEntityToProtoMessage(entity, example)
+	assert.Equal(t, properties["StringKey"].StringValue, example.GetStringKey())
+	assert.Equal(t, entity.Properties["Int64Key"].IntegerValue, example.GetInt64Key())
+	assert.Equal(t, entity.Properties["BoolKey"].BooleanValue, example.GetBoolKey())
+	assert.Equal(t, float64(64), entity.Properties["DoubleKey"].DoubleValue)
 }
