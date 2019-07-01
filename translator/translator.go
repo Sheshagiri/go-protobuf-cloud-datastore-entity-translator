926ffce15f884eb3c3774935bf98b273a3e15b2a
Sheshagiri Rao Mallipedhi <shmallipedhi@extremenetworks.com>
2019-07-01 12:19:00 +0530

    refactor: use reflect.Kind()

--- translator/translator.go
+++ translator/translator.go
@@ -20,13 +20,13 @@ func ProtoMessageToDatastoreEntity(src proto.Message) dbv2.Entity {
 	properties := make(map[string]dbv2.Value)
 
 	for i := 0; i < srcValues.NumField(); i++ {
-		name := srcValues.Type().Field(i).Name
-		if !strings.ContainsAny(name, "XXX_") {
-			fType := srcValues.Field(i).Type().Kind().String()
-			value, err := toValue(fType, srcValues.Field(i))
+		fName := srcValues.Type().Field(i).Name
+		if !strings.ContainsAny(fName, "XXX_") {
+			//fType := srcValues.Field(i).Type().Kind().String()
+			value, err := toValue(srcValues.Field(i))
 			// fmt.Printf("name:%s, type:%v, value:%v\n",name,fType,value)
 			if err == nil {
-				properties[name] = value
+				properties[fName] = value
 			} else {
 				fmt.Printf("err: %v\n", err)
 			}
@@ -42,33 +42,33 @@ func DatastoreEntityToProtoMessage(src dbv2.Entity, dst proto.Message) {
 		fName := dstValues.Type().Field(i).Name
 		if !strings.Contains(fName, "XXX_") {
 			fValue := getProperty(src.Properties, fName)
-			fType := dstValues.Type().Field(i).Type.String()
+			fType := dstValues.Type().Field(i).Type.Kind()
 			fmt.Printf("name: %s, type: %s\n", fName, fType)
 			switch fType {
-			case "string":
+			case reflect.String:
 				dstValues.Field(i).SetString(fValue.StringValue)
-			case "bool":
+			case reflect.Bool:
 				dstValues.Field(i).SetBool(fValue.BooleanValue)
-			case "int32", "int64":
+			case reflect.Int32, reflect.Int64:
 				dstValues.Field(i).SetInt(fValue.IntegerValue)
-			case "float32", "float64":
+			case reflect.Float32, reflect.Float64:
 				dstValues.Field(i).SetFloat(fValue.DoubleValue)
 			}
 		}
 	}
 }
 
-func toValue(fType string, fValue reflect.Value) (value dbv2.Value, err error) {
-	switch fType {
-	case "string":
+func toValue(fValue reflect.Value) (value dbv2.Value, err error) {
+	switch fValue.Kind() {
+	case reflect.String:
 		value.StringValue = fValue.String()
-	case "bool":
+	case reflect.Bool:
 		value.BooleanValue = fValue.Bool()
-	case "int32", "int64":
+	case reflect.Int32, reflect.Int64:
 		value.IntegerValue = fValue.Int()
-	case "float32", "float64":
+	case reflect.Float32, reflect.Float64:
 		value.DoubleValue = fValue.Float()
-	case "slice":
+	case reflect.Slice:
 		//TODO add complex type to the slicell
 		if fValue.Type().Elem().Kind() == reflect.Uint8 {
 			//BlobValue is a string in the datastore entity proto
@@ -77,16 +77,16 @@ func toValue(fType string, fValue reflect.Value) (value dbv2.Value, err error) {
 			size := fValue.Len()
 			values := make([]*dbv2.Value, size)
 			for i := 0; i < size; i++ {
-				val, _ := toValue(fValue.Type().Elem().Kind().String(), fValue.Index(i))
+				val, _ := toValue(fValue.Index(i))
 				values[i] = &val
 			}
 			value.ArrayValue = &dbv2.ArrayValue{
 				Values: values,
 			}
 		}
-	case "map":
+	case reflect.Map:
 		err = errors.New("datatype[map] not supported")
-	case "ptr":
+	case reflect.Ptr:
 		err = errors.New("datatype[ptr] not supported")
 	default:
 		fmt.Println("inside default case")
