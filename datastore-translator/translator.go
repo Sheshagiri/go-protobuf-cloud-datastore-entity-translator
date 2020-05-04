de05aa7110da06f5f3da3fc0556d1588601b098a
Sheshagiri <sheshagiri.mallipedhi@citrix.com>
2020-05-04 16:07:14 +0530

    fix deepssource issue

--- datastore-translator/translator.go
+++ datastore-translator/translator.go
@@ -41,7 +41,7 @@ func ProtoMessageToDatastoreEntity(src proto.Message, snakeCase bool, excludeFro
 		}
 	}
 
-	excludeFields := make(map[string]string, 0)
+	excludeFields := make(map[string]string)
 	for _, fd := range md.GetField() {
 		if fd.GetOptions() != nil {
 			excludeFields[fd.GetName()] = fd.GetOptions().String()
@@ -301,7 +301,7 @@ func fromStructValueToDatastoreValue(v *structpb.Value) *datastore.Value {
 		}
 	case *structpb.Value_StructValue:
 		structValue := v.GetStructValue()
-		properties := make(map[string]*datastore.Value, 0)
+		properties := make(map[string]*datastore.Value)
 		for key, value := range structValue.GetFields() {
 			properties[key] = fromStructValueToDatastoreValue(value)
 		}
